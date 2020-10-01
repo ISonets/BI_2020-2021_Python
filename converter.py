a0b6183af63bd0e09c8b33badd9812bdf1bb1d1a
Ignat Sonets <70810377+ISonets@users.noreply.github.com>
2020-10-01 15:40:21 +0300

    Update converter.py(added new unit)

--- converter.py
+++ converter.py
@@ -1,9 +1,9 @@
 #simple volume converter for buffer making
-#new_version
+#added new unit: drop( useful for pharmacology ex tempore using
 print('Hello!')
 print('This program converts one volume unit into another unit, which is useful for proper buffer making')
-print('Following are the available units: (nl), (mkl), (ml), (l)')
+print('Following are the available units: (nl), (mkl), (ml), (drop), (l)')
 print('This converter uses functions. The syntax is: function(your number,unit to convert from, unit to convert to)')
 def convert_func(val, unit_from, unit_to):
-    SI = {'nl': 0.000000001, 'mkl':0.000001, 'ml':0.001, 'l':1.0,}
+    SI = {'nl': 0.000000001, 'mkl':0.000001, 'ml':0.001, 'drop':0.00005, 'l':1.0}
     return round((val*SI[unit_from]/SI[unit_to]), 5)
