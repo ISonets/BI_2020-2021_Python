0d5c13db46c1a2b852074bdc70fa222139c6908a
Ignat Sonets <70810377+ISonets@users.noreply.github.com>
2020-10-01 20:49:29 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
-#simple calculator, homework №1
-a = int(input()) #first number
-op = input() #operation
-b = int(input()) #second number
+  # simple calculator, homework №1
+a = int(input())  # first number
+op = input()  # operation
+b = int(input())  # second number
 if op == '/' and b == 0:
     print('На ноль делить нельзя!')
 elif op == '/' and b != 0:
