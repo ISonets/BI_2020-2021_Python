c569194ba608c9b374006bdbc239889c1ac76e59
Ignat Sonets <70810377+ISonets@users.noreply.github.com>
2021-05-29 17:05:44 +0300

    Update new.py

--- new.py
+++ new.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 # database init + cursor
 snp_conn = sqlite3.connect("snp.db")
-c = snp_conn.cursor()
+cursor = snp_conn.cursor()
 
 # making db for genstudio
-c.execute('''
+cursor.execute('''
 CREATE TABLE genstudio (
 ID INTEGER PRIMARY KEY,
 SNP_Name TEXT,
@@ -25,27 +25,26 @@ Allele2_AB TEXT,
 Chr INTEGER,
 Position INTEGER);
 ''')
-
 # metadata.db creating
-c.execute('''
+cursor.execute('''
 CREATE TABLE metadata (
 ID INTEGER PRIMARY KEY,
 dna_chip_id TEXT,
 breed TEXT,
-sex TEXT
+sex TEXT,
 FOREIGN KEY (dna_chip_id) REFERENCES genstudio (Sample_ID) ON DELETE CASCADE);
 ''')
 
 # stats table
-c.execute('''CREATE TABLE stats (
+cursor.execute('''CREATE TABLE stats (
 sample_id INTEGER,
-SNP_Index INTEGER
+SNP_Index INTEGER,
 GC_Score REAL,
 GT_Score REAL,
 Theta REAL,
 R REAL,
 B_Allele_Freq REAL,
-Log_R_Ratio REAL
+Log_R_Ratio REAL,
 FOREIGN KEY (sample_id) REFERENCES metadata (sample_id) ON DELETE CASCADE,
 FOREIGN KEY (SNP_Index) REFERENCES genstudio (SNP_Index) ON DELETE CASCADE);
 ''')
@@ -54,9 +53,46 @@ FOREIGN KEY (SNP_Index) REFERENCES genstudio (SNP_Index) ON DELETE CASCADE);
 genstudio = pd.read_csv("genstudio.csv")
 metadata = pd.read_csv("metadata.csv")
 
+genstudio = genstudio.drop('Unnamed: 0', axis=1)
+metadata = metadata.drop('Unnamed: 0', axis=1)
+
+genstudio.rename(columns = {'SNP Name':'SNP_Name',
+ 'SNP Index':'SNP_Index',
+ 'SNP Aux':'SNP_Aux',
+ 'Sample ID':'Sample_ID',
+ 'Allele1 - Top':'Allele1_Top',
+ 'Allele2 - Top':'Allele2_Top',
+ 'Allele1 - Forward':'Allele1_Forward',
+ 'Allele2 - Forward':'Allele2_Forward',
+ 'Allele1 - AB':'Allele1_AB',
+ 'Allele2 - AB':'Allele2_AB',
+ 'GC Score':'GC_Score',
+ 'GT Score':'GT_Score',
+ 'B Allele Freq':'B_Allele_Freq',
+ 'Log R Ratio':'Log_R_Ratio'}, inplace = True)
+ 
+stats = genstudio[['SNP_Index','Sample_ID','GC_Score',
+                   'GT_Score','B_Allele_Freq','R','Theta','Log_R_Ratio']]
+                   
+genstudio = genstudio.drop(['GC_Score', 'GT_Score','B_Allele_Freq','R','Theta','Log_R_Ratio'],axis=1)
+                                
+genstudio.to_csv('genstudio_mod.csv') 
+metadata.to_csv('metadata.csv')
+stats.to_csv('stats.csv')
+
+genstudio_mod = pd.read_csv('genstudio_mod.csv')
+genstudio_mod = genstudio_mod.drop('Unnamed: 0', axis=1)
 
 ### filling
-genstudio.to_sql('genstudio', con=snp_conn, if_exists='append', index = False)
-metadata.to_sql('metadata', con=meta_conn, if_exists='append', index = False)
+genstudio_mod.to_sql('genstudio', con=snp_conn, if_exists='append', index = False)
+metadata.to_sql('metadata', con=snp_conn, if_exists='append', index = False)
+stats.to_sql('stats', con=snp_conn, if_exists='append', index = False)
 snp_conn.commit()
-conn.close()
+# conn.close()
+
+# trying
+sql_delete_query = """Delete from genstudio where SNP_Name = '1_10723065'"""
+sql_update_query = """Update metadata set sex = 'XY' where dna_chip_id = '202341831114R02C01'"""
+sql_cascade_query = """Delete from link_table_cascade where sample_id = '1'"""
+snp_conn.commit
+snp_conn.close()
